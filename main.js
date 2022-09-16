a038babfd16a1c759b919baff8412352bfe35803
Aishik <aishikd2@gmail.com>
2022-09-16 13:29:05 +0530

    v1.2

--- main.js
+++ main.js
@@ -151,3 +151,13 @@ document.addEventListener("keydown", (e) => {
   else if (e.key == "Backspace") deleteOperation();
   else return;
 });
+
+window.addEventListener('load', () => { 
+  document.querySelector(':root').style
+    .setProperty('--vh', window.innerHeight/100 + 'px');
+})
+
+window.addEventListener('resize', () => { 
+  document.querySelector(':root').style
+    .setProperty('--vh', window.innerHeight/100 + 'px');
+})
